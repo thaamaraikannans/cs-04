7adb182d1b8493723b4134176a348a99b08eecb8
thaamaraikannan <thaamaraikannans.xbi4@gmail.com>
2024-12-05 19:24:00 +0530

    till task 1

---
+++ module1/loops/for.py
@@ -0,0 +1,18 @@
+# interatable value could be any one of sequential values
+# list, string, tuple, range....
+
+
+# value = "1 million", "2 million", "3 million"
+# print(type(value))
+
+# for a in value:
+#     print(a)
+#     print(type(a))
+
+# print(a)
+
+
+# want to perform iteration for number of times alone -> range
+
+for value in range(0, 10):
+    print(f" hello world {value}")
---
+++ module1/operators/comp.py
@@ -0,0 +1,16 @@
+# age = 18
+# is_having_voter = True
+
+# if age >= 18 or is_having_voter:
+#     print("eligible to vote")
+# else:
+#     print("not elogible to vote")
+
+# if age >= 18 and is_having_voter :
+#     print("go for to polling")
+# else:
+#     print("don't go for polling")
+
+value = True
+
+print(not value)
